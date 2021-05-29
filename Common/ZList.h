9fa9bcd1406360df7848eabcdd0d4b8777adb186
Erik A <minimumdelta@gmail.com>
2021-05-28 23:58:49 -0500

    begin zlist implementation removed some unused includes

--- Common/ZList.h
+++ Common/ZList.h
@@ -1,72 +1,251 @@
 #pragma once
+#include "ZRefCountedAccessor.h"
+#include "ZRefCountedDummy.h"
 
-struct ZListDataVtbl
-{
-    void* (__thiscall* __vecDelDtor)(unsigned int);
-};
-
+/// <summary>
+/// Emulation of MapleStory's implementation of a doubly linked list template.
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
 template <typename T>
-struct ZListData
-{
-    ZListDataVtbl* vfptr;
-    ZListData<T>* pNext;
-    ZListData<T>* pPrev;
-    char aPad[4];
-    T data;
-};
-
-template <typename T>
-class ZList
+class ZList : ZRefCountedAccessor<T>, ZRefCountedAccessor<ZRefCountedDummy<T>>
 {
 private:
-    void* vfptr;
-    void* padding;
-
+	char gap4[1];
+	size_t m_uCount;
+	T* m_pHead;
+	T* m_pTail;
 public:
-    unsigned int m_uCount;
-    T* m_pHead;
-    T* m_pTail;
 
-    ZList()
-    {
-        vfptr = 0;
-        padding = 0;
+	/***=========== CONSTRUCTORS ===========***/
 
-        m_uCount = 0;
-        m_pHead = 0;
-        m_pTail = 0;
-    }
+	ZList()
+	{
+		m_uCount = 0;
+		m_pHead = nullptr;
+		m_pTail = nullptr;
+	}
 
-    T* GetNext(T** ppPosition)
-    {
-        T* pResult = *ppPosition;
+	ZList(ZList<T>* l)
+	{
+		m_uCount = 0;
+		m_pHead = nullptr;
+		m_pTail = nullptr;
 
-        ZListData<T>* pData = reinterpret_cast<ZListData<T>*>(reinterpret_cast<unsigned char*>(pResult) - 16);
+		this->RemoveAll();
+		this->AddTail(l);
+	}
 
-        if (pData->pNext)
-            *ppPosition = &pData->pNext->data;
-        else
-            *ppPosition = nullptr;
+	virtual ~ZList()
+	{
+		this->RemoveAll();
+	}
 
-        return pResult;
-    }
+	/***=========== OPERATOR OVERLOAD ===========***/
 
-    void RemoveAll()
-    {
-        auto pPosition = this->m_pHead;
+	ZList<T>* operator=(ZList<T>* l)
+	{
+		this->RemoveAll();
+		this->AddTail(l);
 
-        while (pPosition)
-        {
-            T* pItem = this->GetNext(&pPosition);
+		return this;
+	}
 
-            ZListData<T>* pData = reinterpret_cast<ZListData<T>*>(reinterpret_cast<unsigned char*>(pItem) - 16);
+	/***=========== MEMBER VARIABLE ACCESSORS ===========***/
 
-            pData->vfptr->__vecDelDtor(1);
+	T* GetHeadPosition()
+	{
+		return this->m_pHead;
+	}
 
-        }
+	T* GetTailPosition()
+	{
+		return this->m_pTail;
+	}
 
-        this->m_pTail = 0;
-        this->m_pHead = 0;
-        this->m_uCount = 0;
-    }
+	size_t GetCount()
+	{
+		return this->m_uCount;
+	}
+
+	/***=========== ADD HEAD ===========***/
+
+	T* AddHead()
+	{
+		T* pAlloc = this->New(this->m_pHead, nullptr);
+
+		if (!this->m_pTail)
+		{
+			this->m_pTail = pAlloc;
+		}
+		else
+		{
+			ZRefCountedDummy<T>* pNode = pAlloc ? this->CastNode(pAlloc) : nullptr;
+			ZRefCountedDummy<T>* pHeadNode = this->CastNode(this->m_pHead);
+
+			pHeadNode->m_pNext = pNode;
+		}
+
+		this->m_pHead = pAlloc;
+
+		return pAlloc;
+	}
+
+	T* AddHead(T* d)
+	{
+		T* pNewHead = this->AddHead();
+		*pNewHead = *d;
+		return pNewHead;
+	}
+
+	T* AddHead(ZList<T>* l)
+	{
+		return nullptr; // TODO
+	}
+
+	/***=========== ADD TAIL ===========***/
+
+	T* AddTail()
+	{
+		T* pAlloc = this->New(this->m_pTail, nullptr);
+
+		if (this->m_pTail)
+		{
+			ZRefCountedDummy<T>* pNode = pAlloc ? this->CastNode(pAlloc) : nullptr;
+			ZRefCountedDummy<T>* pTailNode = this->CastNode(this->m_pTail);
+
+			pTailNode->m_pNext = pNode;
+			this->m_pTail = pAlloc;
+		}
+		else
+		{
+			this->m_pHead = pAlloc;
+			this->m_pTail = pAlloc;
+		}
+
+		return pAlloc;
+	}
+
+	T* AddTail(T* d)
+	{
+		T* pNewTail = this->AddTail();
+		*pNewTail = *d;
+		return pNewTail;
+	}
+
+	T* AddTail(ZList<T>* l)
+	{
+		T* pHead = l->m_pHead;
+
+		while (pHead)
+		{
+			T* pNext = pHead;
+
+			ZRefCountedDummy<T>* pNode = this->CastNode(pHead);
+			ZRefCountedDummy<T>* pNodePrev = this->CastNode(pNode->m_pPrev);
+
+			pHead = pNodePrev ? pNodePrev->t : nullptr;
+
+			T* pNew = this->AddTail();
+			*pNew = *pNext;
+		}
+	}
+
+	/***=========== NODE REMOVAL ===========***/
+
+	void RemoveAll()
+	{
+		T* pPosition = this->GetHeadPosition();
+
+		while (pPosition)
+		{
+			T* pItem = this->GetNext(&pPosition);
+
+			pItem->~T();
+		}
+
+		this->m_pTail = nullptr;
+		this->m_pHead = nullptr;
+		this->m_uCount = 0;
+	}
+
+	void RemoveAt(T* pos)
+	{
+		// TODO
+	}
+
+	/***=========== NODE SEARCH ===========***/
+
+	T* FindIndex(const size_t uIndex)
+	{
+		// TODO
+	}
+
+	int IndexOf(T* pos)
+	{
+		// TODO
+	}
+
+	/***=========== INSERTION ===========***/
+
+	T* Insert(T* d)
+	{
+		T* result = this->AddTail(this);
+	}
+
+	T** InsertBefore(T* pos)
+	{
+
+	}
+
+	/***=========== TRAVERSAL ===========***/
+
+	T* GetNext(T** pos)
+	{
+		T* pRet = *pos;
+
+		ZRefCountedDummy<T>* pNode = this->CastNode(pRet);
+
+		*pos = pNode->m_pNext ? &this->CastNode((T*)pNode->m_pNext)->t : nullptr;
+
+		return pRet;
+	}
+
+	T* GetPrev(T** pos)
+	{
+		T* pRet = *pos;
+
+		ZRefCountedDummy<T>* pNode = this->CastNode(pRet);
+
+		*pos = pNode->m_pPrev ? &this->CastNode((T*)pNode->m_pPrev)->t : nullptr;
+
+		return pRet;
+	}
+
+private:
+
+	/// <summary>
+	/// If t is not a ZList member, then this will produce undefined results
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	ZRefCountedDummy<T>* CastNode(T* t)
+	{
+		return reinterpret_cast<ZRefCountedDummy<T>*>(reinterpret_cast<char*>(t) - 16);
+	}
+
+	T* New(T* pPrev, T* pNext)
+	{
+		ZRefCountedDummy<T>* pAlloc = new ZRefCountedDummy<T>(); // IDA: ZRefCounted_Alloc<ZRefCountedDummy<T>>();
+
+		pAlloc->m_pPrev = pPrev ? this->CastNode(pPrev) : nullptr;
+		pAlloc->m_pNext = pNext ? this->CastNode(pNext) : nullptr;
+		pAlloc->m_nRef = 0;
+
+		this->m_uCount += 1;
+
+		return &pAlloc->t;
+	}
 };
+
+assert_size(sizeof(ZList<int>), 0x14);
--- TemplateTesting/TemplateTesting.cpp
+++ TemplateTesting/TemplateTesting.cpp
@@ -9,84 +9,30 @@
 #include "ZtlSecure.h"
 #include <TSecType.h>
 #include "ZArray.h"
-
-struct SizeTest10
-{
-public:
-	char aPad[10];
-};
-struct SizeTest20
-{
-public:
-	char aPad[20];
-};
-struct SizeTest40
-{
-public:
-	char aPad[40];
-};
-struct SizeTest80
-{
-public:
-	char aPad[80];
-};
-struct SizeTest160
-{
-public:
-	char aPad[160];
-};
-struct SizeTest320
-{
-public:
-	char aPad[320];
-};
+#include "ZList.h"
 
 int main()
 {
-	std::cout << "test begin\r\n";
-	ZArray<SizeTest10> arr1 = ZArray<SizeTest10>();
-	ZArray<SizeTest20> arr2 = ZArray<SizeTest20>();
-	ZArray<SizeTest40> arr3 = ZArray<SizeTest40>();
-	ZArray<SizeTest80> arr4 = ZArray<SizeTest80>();
-	ZArray<SizeTest160> arr5 = ZArray<SizeTest160>();
-	ZArray<SizeTest320> arr6 = ZArray<SizeTest320>();
+	auto list = ZList<int>();
 
-	for (int i = 0; i < 100; i++)
+	int poo = 40;
+	int pee = 30;
+	int paa = 20;
+	int poo2 = 10;
+
+	list.AddTail(&poo);
+	list.AddTail(&pee);
+	list.AddTail(&paa);
+	list.AddTail(&poo2);
+
+	//list.AddHead(&poo2);
+
+	PINT peepaa = list.GetHeadPosition();
+	while (peepaa)
 	{
-		for (int j = 0; j < 2500; j++)
-		{
-			switch (rand() % 6)
-			{
-			case 0:
-				arr1.InsertBefore();
-				break;
-			case 1:
-				arr2.InsertBefore();
-				break;
-			case 2:
-				arr3.InsertBefore();
-				break;
-			case 3:
-				arr4.InsertBefore();
-				break;
-			case 4:
-				arr5.InsertBefore();
-				break;
-			case 5:
-				arr6.InsertBefore();
-				break;
-			}
-		}
-
-		std::cout << "arr1 size: " << arr1.GetCount() << "\r\n";
-
-		arr1.RemoveAll();
-		arr2.RemoveAll();
-		arr3.RemoveAll();
-		arr4.RemoveAll();
-		arr5.RemoveAll();
-		arr6.RemoveAll();
+		peepaa = list.GetNext(&peepaa); // TODO fix iteration
 	}
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
